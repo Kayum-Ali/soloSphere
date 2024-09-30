0e78670aa6699e2a699d7761eeb28e90515e6ca0
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-09-30 23:46:09 +0600

    dynamic carousel slider added in swiper slide

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,18 +9,23 @@ import 'swiper/css/navigation';
 
 import './styles.css';
 
+import bgImg1 from '../assets/images/carousel1.jpg'
+import bgImg2 from '../assets/images/carousel2.jpg'
+import bgImg3 from '../assets/images/carousel3.jpg'
+
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Slide from './Slide';
 
 const Carousel=()=> {
   return (
-    <>
+    <div className='container px-6 py-10 mx-auto'>
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
         loop={true}
         autoplay={{
-          delay: 2500,
+          delay: 3000,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -30,17 +35,18 @@ const Carousel=()=> {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+            <Slide image={bgImg1} text={'Get your web development projects done in minutes'}></Slide>
+        </SwiperSlide>
+        <SwiperSlide>
+            <Slide image={bgImg2} text={'Get your Graphics Design projects done in minutes'}></Slide>
+        </SwiperSlide>
+        <SwiperSlide>
+            <Slide  image={bgImg3} text={'Start your Digital Markting Compain up in running'}></Slide>
+        </SwiperSlide>
+        
       </Swiper>
-    </>
+    </div>
   );
 }
 
