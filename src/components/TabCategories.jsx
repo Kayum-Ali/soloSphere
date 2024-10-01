9c409760f1e72c10c34bc50ae7be4aafe3f4919f
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-10-01 09:24:19 +0600

    when clicked react tabs then show JobCard

--- src/components/TabCategories.jsx
+++ src/components/TabCategories.jsx
@@ -1,30 +1,33 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import JobCard from "./JobCard";
 
 const TabCategories = () => {
   return (
-    <div className="container mx-auto">
-      <Tabs>
-        <h2>Browse jobs by Categori</h2>
-      <div className="flex items-center justify-center">
-      <TabList>
-          <Tab>Web Development</Tab>
-          <Tab>Graphics  Design</Tab>
-          <Tab>Digital Marktng</Tab>
-        </TabList>
-      </div>
+    <Tabs>
+      <div className="container py-10 px-6 mx-auto">
+
+        <h2 className="text-2xl lg:text-3xl text-center text-gray-800 capitalize font-semibold">Browse jobs by Categori</h2>
+        <p className="max-w-2xl mx-auto my-6 text-center text-gray-500">Three Categories available for the time being. They are web development, Graphics design and Digital Matketing. Browse them by clicking on the tab bellow</p>
+        <div className="flex items-center justify-center">
+          <TabList>
+            <Tab>Web Development</Tab>
+            <Tab>Graphics Design</Tab>
+            <Tab>Digital Marktng</Tab>
+          </TabList>
+        </div>
 
         <TabPanel>
-          <h2>Web Development</h2>
+           <JobCard></JobCard>
         </TabPanel>
         <TabPanel>
-          <h2>Graphics  Design</h2>
+           <JobCard></JobCard>
         </TabPanel>
         <TabPanel>
-          <h2>Digital Marktng</h2>
+           <JobCard></JobCard>
         </TabPanel>
-      </Tabs>
-    </div>
+      </div>
+    </Tabs>
   );
 };
 
