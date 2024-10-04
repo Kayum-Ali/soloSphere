1bde4e60a06055bc4fa622eba66de3d7dd05b35e
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-10-04 22:42:36 +0600

    use tanStack query

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,15 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes'
 import AuthProvider from './provider/AuthProvider'
 import  { Toaster } from 'react-hot-toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <AuthProvider>
-       <RouterProvider router={router}></RouterProvider>
+       <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router}></RouterProvider>
+       </QueryClientProvider>
        <Toaster />
    </AuthProvider>
   </StrictMode>,
