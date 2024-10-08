6c772695aeee881369d1aff0c3c3dc3834e921da
Kayum <sisirfahimstudent173@gmail.com>
2024-10-08 10:27:48 +0600

    fetch all job in database

--- src/Pages/AllJobs.jsx
+++ src/Pages/AllJobs.jsx
@@ -1,6 +1,23 @@
-// import JobCard from '../components/JobCard'
+import JobCard from '../components/JobCard'
+
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 const AllJobs = () => {
+    const [jobs, setJobs] = useState([])
+    const [loadings, setLoading] = useState(true)
+  
+    useEffect(()=>{
+      setLoading(true)
+      const getData = async()=>{
+        const data = await axios(`${import.meta.env.VITE_API_URL}/jobs`)
+        setJobs(data.data)
+      }
+      getData()
+      setLoading(false)
+    },[])
+
+    console.log(jobs)
   const pages = [1, 2, 3, 4, 5]
   return (
     <div className='container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between'>
@@ -48,9 +65,9 @@ const AllJobs = () => {
           <button className='btn'>Reset</button>
         </div>
         <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-          {/* {jobs.map(job => (
+          {jobs.map(job => (
             <JobCard key={job._id} job={job} />
-          ))} */}
+          ))}
         </div>
       </div>
 
